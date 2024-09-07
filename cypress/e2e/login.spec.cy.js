246aced8272d2e4608c2aca5b32f7bbf03aec463
vladimir <vladimirjuridico21@gmail.com>
2024-09-07 17:37:13 -0300

    Adding user data json

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -10,19 +10,9 @@ describe('Orange HRM Tests', () => {
     dashboardGrid: ".orangehrm-dashboard-grid",
     wrongCredentialAlert: "[role='alert']"
   }
-const userData = {
-  userSucess: {
-    username: 'Admin',
-    password: 'admin123'
-  },
-  userfail: {
-    username: 'teste',
-    password: 'teste'
-  }
-}
 
   it('Login success', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSucess.username)
     cy.get(selectorsList.passwordField).type(userData.userSucess.password)
     cy.get(selectorsList.loginButton).click()
@@ -30,7 +20,7 @@ const userData = {
     cy.get(selectorsList.dashboardGrid)
   })
   it('Login - Fail', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSucess.username)
     cy.get(selectorsList.passwordField).type(userData.userSucess.password)
     cy.get(selectorsList.loginButton).click()
